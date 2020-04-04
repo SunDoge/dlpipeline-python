b51b46d8a7652f2f8ce6325f01d9d8a9820db3de
SunDoge <384813529@qq.com>
2020-04-04 13:39:40 +0800

    add functional

--- dlpipeline/image/transforms/functional.py
+++ dlpipeline/image/transforms/functional.py
@@ -1,2 +1,9 @@
 import pyvips
+import logging
+
+logger = logging.getLogger(__name__)
+
+def pyvips_loader(path: str, access=pyvips.Access.SEQUENTIAL):
+    return pyvips.Image.new_from_file(path, access=access)
+
 
